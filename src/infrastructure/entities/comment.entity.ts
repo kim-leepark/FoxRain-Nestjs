3c3f3457ad9033967881ddb4bf51064246709e43
Seung jun <poodle2405@gmail.com>
2022-04-20 15:48:29 +0900

    ⚡️ :: (#2) inf comment entity 설정

--- src/infrastructure/entities/comment.entity.ts
+++ src/infrastructure/entities/comment.entity.ts
@@ -1,7 +1,38 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { CommentReportTypeOrmEntity } from './comment-report.entity';
+import { PostTypeOrmEntity } from './post.entity';
+import { UserTypeOrmEntity } from './user.entity';
 
 @Entity('comment')
 export class CommentTypeOrmEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ length: 100 })
+  content: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @ManyToOne(() => UserTypeOrmEntity, (user) => user.comments)
+  @JoinColumn({ name: 'user_id' })
+  user: UserTypeOrmEntity;
+
+  @ManyToOne(() => PostTypeOrmEntity, (post) => post.comments)
+  @JoinColumn({ name: 'post_id' })
+  post: PostTypeOrmEntity;
+
+  @OneToMany(
+    () => CommentReportTypeOrmEntity,
+    (comment_report) => comment_report.comment,
+  )
+  comment_report: CommentReportTypeOrmEntity[];
 }
