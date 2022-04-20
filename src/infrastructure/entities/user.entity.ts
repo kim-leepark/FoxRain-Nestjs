1bc90ede4b468cf241fc8e735d5a2f3078e09f98
Seung jun <poodle2405@gmail.com>
2022-04-20 15:49:00 +0900

    ⚡️ :: (#2) inf user entity 설정

--- src/infrastructure/entities/user.entity.ts
+++ src/infrastructure/entities/user.entity.ts
@@ -1,7 +1,28 @@
-import { Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { CommentReportTypeOrmEntity } from './comment-report.entity';
+import { CommentTypeOrmEntity } from './comment.entity';
+import { PostTypeOrmEntity } from './post.entity';
 
 @Entity('user')
 export class UserTypeOrmEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ length: 50 })
+  email: string;
+
+  @Column({ length: 30 })
+  name: string;
+
+  @OneToMany(() => CommentTypeOrmEntity, (comment) => comment.user)
+  comments: CommentTypeOrmEntity[];
+
+  @OneToMany(
+    () => CommentReportTypeOrmEntity,
+    (comment_report) => comment_report.user,
+  )
+  comment_report: CommentReportTypeOrmEntity[];
+
+  @OneToMany(() => PostTypeOrmEntity, (post) => post.user)
+  posts: PostTypeOrmEntity[];
 }
