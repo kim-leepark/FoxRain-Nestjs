3daec4772e345551ee5162817f5083e3af93198e
Seung jun <poodle2405@gmail.com>
2022-04-20 15:46:19 +0900

    ⚡️ :: (#2) domain comment

--- src/domain/model/comment.ts
+++ src/domain/model/comment.ts
@@ -1,3 +1,13 @@
+import { Expose } from 'class-transformer';
+
 export class Comment {
   id: number;
+  content: string;
+  created_at: Date;
+
+  @Expose({ name: 'user_id' })
+  userId: number;
+
+  @Expose({ name: 'post_id' })
+  postId: number;
 }
