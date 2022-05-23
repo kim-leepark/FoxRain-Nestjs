edb9db683c5030489aceb9b10a79047e0e29f6d8
Seung jun <poodle2405@gmail.com>
2022-05-23 19:40:41 +0900

    ⚡️ :: findOne, CommentReport 기능 추가

--- src/infrastructure/repositories/comment-report.repotiroty.ts
+++ src/infrastructure/repositories/comment-report.repotiroty.ts
@@ -1,15 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CommentReportM } from 'src/domain/model/comment-report';
 import { CommentReportRepository } from 'src/domain/repositories/comment-report.repository';
 import { Repository } from 'typeorm';
 import { CommentReportTypeOrmEntity } from '../entities/comment-report.entity';
+import { CommentTypeOrmEntity } from '../entities/comment.entity';
+import { UserTypeOrmEntity } from '../entities/user.entity';
 
 @Injectable()
-export class DatabaseCommentReportRepository
-  implements CommentReportRepository
-{
+export class DatabaseCommentReportRepository implements CommentReportRepository {
   constructor(
     @InjectRepository(CommentReportTypeOrmEntity)
     private readonly commentReportEntityRepository: Repository<CommentReportTypeOrmEntity>,
+    @InjectRepository(CommentTypeOrmEntity)
+    private readonly commentEntityRepository: Repository<CommentTypeOrmEntity>,
+    @InjectRepository(UserTypeOrmEntity)
+    private readonly userEntityRepository: Repository<UserTypeOrmEntity>,
   ) {}
+
+  async findOne(commentId: number, userId: number) {
+    return await this.commentReportEntityRepository
+      .createQueryBuilder('comment_report')
+      .select()
+      .where('comment_report.comment_id = :comment_id', { comment_id: commentId })
+      .andWhere('comment_report.user_id = :user_id', { user_id: userId })
+      .getOne();
+  }
+
+  async commentReport(content: string, commentId: number, userId: number): Promise<void> {
+    const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(userId);
+    const comment: CommentTypeOrmEntity = await this.commentEntityRepository.findOne(commentId);
+
+    await this.commentReportEntityRepository.save({
+      content,
+      user,
+      comment,
+    });
+  }
 }
