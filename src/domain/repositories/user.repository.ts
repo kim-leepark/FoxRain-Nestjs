ac693046ac9bf1276c6faf6a529e839eca85a1ff
Seung jun <poodle2405@gmail.com>
2022-05-23 19:16:57 +0900

    :recycle: :: code format

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -4,9 +4,5 @@ import { SignUpDto } from 'src/presentation/user/user.dto';
 export interface UserRepository {
   findOne(email: string);
   singUp(request: SignUpDto): Promise<void>;
-  userInfo(
-    userId: number,
-    page: number,
-    size: number,
-  ): Promise<GetUserInfoPresenter[]>;
+  userInfo(userId: number, page: number, size: number): Promise<GetUserInfoPresenter[]>;
 }
