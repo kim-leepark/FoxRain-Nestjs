4d24ab2a84bf42f91f5ced7a3f4585086aba5e6f
Seung jun <poodle2405@gmail.com>
2022-05-23 10:04:48 +0900

    ⚡️ :: user sign-up, findOne 기능 추가

--- src/infrastructure/repositories/user.repotiroty.ts
+++ src/infrastructure/repositories/user.repotiroty.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/domain/model/user';
 import { UserRepository } from 'src/domain/repositories/user.repository';
 import { Repository } from 'typeorm';
 import { UserTypeOrmEntity } from '../entities/user.entity';
@@ -10,4 +11,14 @@ export class DatabaseUserRepository implements UserRepository {
     @InjectRepository(UserTypeOrmEntity)
     private readonly userEntityRepository: Repository<UserTypeOrmEntity>,
   ) {}
+
+  findOne(email: string) {
+    return this.userEntityRepository.findOne({ email: email });
+  }
+
+  async singUp(request: User): Promise<void> {
+    const user = this.userEntityRepository.create(request);
+
+    this.userEntityRepository.save(user);
+  }
 }
