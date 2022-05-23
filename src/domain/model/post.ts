7b8150bd6d8af61c77d25e79439722c20a02adc7
Seung jun <poodle2405@gmail.com>
2022-05-23 11:21:52 +0900

    :recycle: :: renaming

--- src/domain/model/post.ts
+++ src/domain/model/post.ts
@@ -1,11 +1,16 @@
 import { Expose } from 'class-transformer';
 
-export class Post {
+export class PostM {
   id: number;
   title: string;
   content: string;
   created_at: Date;
+  name: string;
 
   @Expose({ name: 'user_id' })
   userId: number;
+
+  constructor(obj) {
+    return Object.assign(this, obj);
+  }
 }
