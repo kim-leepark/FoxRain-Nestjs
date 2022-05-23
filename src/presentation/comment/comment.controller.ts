6e751b1cf44bd671974e4375f2b3bcedd103ede9
Seung jun <poodle2405@gmail.com>
2022-05-23 19:41:13 +0900

    ⚡️ :: Post /comment/:commentId/report 추가

--- src/presentation/comment/comment.controller.ts
+++ src/presentation/comment/comment.controller.ts
@@ -1,21 +1,9 @@
-import {
-  Body,
-  Controller,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Inject,
-  Param,
-  ParseIntPipe,
-  Post,
-  Query,
-  Scope,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Param, ParseIntPipe, Post, Query, Scope, UseGuards } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IUserReqeust } from 'src/domain/interfaces/request.interface';
 import { CommentM } from 'src/domain/model/comment';
+import { CommentReportUsecase } from 'src/usecase/comment/comment-report';
 import { CreateCommentUsecase } from 'src/usecase/comment/create-comment';
 import { GetCommentListUsecase } from 'src/usecase/comment/get-comment-list';
 
@@ -26,6 +14,8 @@ export class CommnetController {
     private readonly createCommentUsecase: CreateCommentUsecase,
     @Inject(GetCommentListUsecase)
     private readonly getCommentListUsecase: GetCommentListUsecase,
+    @Inject(CommentReportUsecase)
+    private readonly commentReportUsecase: CommentReportUsecase,
     @Inject(REQUEST)
     private readonly request: IUserReqeust,
   ) {}
@@ -33,15 +23,8 @@ export class CommnetController {
   @UseGuards(AuthGuard('jwt'))
   @Post('/:postId')
   @HttpCode(HttpStatus.CREATED)
-  createComment(
-    @Body('content') content: string,
-    @Param('postId', ParseIntPipe) postId: number,
-  ) {
-    return this.createCommentUsecase.execute(
-      content,
-      postId,
-      this.request.user.sub,
-    );
+  createComment(@Body('content') content: string, @Param('postId', ParseIntPipe) postId: number) {
+    return this.createCommentUsecase.execute(content, postId, this.request.user.sub);
   }
 
   @Get('/:postId')
@@ -51,5 +34,12 @@ export class CommnetController {
     @Query('size', ParseIntPipe) size: number,
   ): Promise<CommentM[]> {
     return this.getCommentListUsecase.execute(postId, page, size);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('/:commentId/report')
+  @HttpCode(HttpStatus.CREATED)
+  commentReport(@Body('content') content: string, @Param('commentId', ParseIntPipe) commentId: number) {
+    return this.commentReportUsecase.execute(content, commentId, this.request.user.sub);
   }
 }
