2493bacc728485e2617f2f72b87b85fd7025d1f5
Seung jun <poodle2405@gmail.com>
2022-05-23 10:04:17 +0900

    ⚡️ :: user usecase 등록

--- src/infrastructure/usecases-proxy/usecases-proxy.module.ts
+++ src/infrastructure/usecases-proxy/usecases-proxy.module.ts
@@ -1,8 +1,12 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { LoginUsecase } from 'src/usecase/user/login';
+import { SignUpUsecase } from 'src/usecase/user/sign-up';
 import { ExceptionsModule } from '../exceptions/exceptions.module';
+import { ExceptionsService } from '../exceptions/exceptions.service';
 import { LoggerModule } from '../logger/logger.module';
 import { RepositoriesModule } from '../repositories/repositories.module';
+import { DatabaseUserRepository } from '../repositories/user.repotiroty';
 
 @Module({
   imports: [
@@ -16,8 +20,31 @@ export class UsecasesProxyDynamicModule {
   static register(): DynamicModule {
     return {
       module: UsecasesProxyDynamicModule,
-      providers: [],
-      exports: [],
+      providers: [
+        {
+          inject: [DatabaseUserRepository, ExceptionsService],
+          provide: SignUpUsecase,
+          useFactory: (
+            databaseUserRepository: DatabaseUserRepository,
+            exceptionsService: ExceptionsService,
+          ) => new SignUpUsecase(databaseUserRepository, exceptionsService),
+        },
+        {
+          inject: [DatabaseUserRepository, ExceptionsService, JwtService],
+          provide: LoginUsecase,
+          useFactory: (
+            databaseUserRepository: DatabaseUserRepository,
+            exceptionsService: ExceptionsService,
+            jwtService: JwtService,
+          ) =>
+            new LoginUsecase(
+              databaseUserRepository,
+              exceptionsService,
+              jwtService,
+            ),
+        },
+      ],
+      exports: [SignUpUsecase, LoginUsecase],
     };
   }
 }
