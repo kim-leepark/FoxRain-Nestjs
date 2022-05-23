16f4e20a4489eaf6a6fb1cb9a35021266ad42179
Seung jun <poodle2405@gmail.com>
2022-05-23 11:23:50 +0900

    ⚡️ :: Get /:userId?page=&size= 추가

--- src/presentation/user/user.controller.ts
+++ src/presentation/user/user.controller.ts
@@ -1,20 +1,27 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Inject,
+  Param,
+  ParseIntPipe,
   Post,
+  Query,
 } from '@nestjs/common';
 import { LoginUsecase } from 'src/usecase/user/login';
 import { SignUpUsecase } from 'src/usecase/user/sign-up';
 import { LoginDto, SignUpDto } from 'src/presentation/user/user.dto';
+import { UserInfoUsecase } from 'src/usecase/user/user-info';
+import { GetUserInfoPresenter } from './user-info.presenter';
 
 @Controller('/user')
 export class UserController {
   constructor(
     @Inject(SignUpUsecase) private readonly signUpUsecase: SignUpUsecase,
     @Inject(LoginUsecase) private readonly loginUsecase: LoginUsecase,
+    @Inject(UserInfoUsecase) private readonly userInfoUsecase: UserInfoUsecase,
   ) {}
 
   @Post('/signup')
@@ -27,5 +34,14 @@ export class UserController {
   @HttpCode(HttpStatus.OK)
   async login(@Body() request: LoginDto) {
     return await this.loginUsecase.execute(request);
+  }
+
+  @Get('/:userId')
+  userInfo(
+    @Param('userId', ParseIntPipe) userId: number,
+    @Query('page', ParseIntPipe) page: number,
+    @Query('size', ParseIntPipe) size: number,
+  ): Promise<GetUserInfoPresenter[]> {
+    return this.userInfoUsecase.execute(userId, page, size);
   }
 }
