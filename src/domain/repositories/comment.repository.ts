ac5b544014cff6c9249698dec58e0344aac28e5a
Seung jun <poodle2405@gmail.com>
2022-05-23 19:16:44 +0900

    ⚡️ :: findCommentList 기능 추가

--- src/domain/repositories/comment.repository.ts
+++ src/domain/repositories/comment.repository.ts
@@ -1,3 +1,6 @@
+import { CommentM } from '../model/comment';
+
 export interface CommentRepository {
   createComment(content: string, postId: number, userId: number): Promise<void>;
+  findCommentList(postId: number, page: number, size: number): Promise<CommentM[]>;
 }
