8b8e7922969ea1b56e8e29aa18543c660393b2e0
Seung jun <poodle2405@gmail.com>
2022-05-23 11:22:18 +0900

    ⚡️ :: userInfo 기능 추가

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,6 +1,12 @@
+import { GetUserInfoPresenter } from 'src/presentation/user/user-info.presenter';
 import { SignUpDto } from 'src/presentation/user/user.dto';
 
 export interface UserRepository {
   findOne(email: string);
   singUp(request: SignUpDto): Promise<void>;
+  userInfo(
+    userId: number,
+    page: number,
+    size: number,
+  ): Promise<GetUserInfoPresenter[]>;
 }
