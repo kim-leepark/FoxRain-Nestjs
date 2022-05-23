986305e78f674eedd50062adf50b42bc169f3324
Seung jun <poodle2405@gmail.com>
2022-05-23 12:07:25 +0900

    ⚡️ :: import JwtModule, providers JwtStrategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from '@nestjs/common';
-import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
 import { LoggerModule } from './infrastructure/logger/logger.module';
 import { ExceptionsModule } from './infrastructure/exceptions/exceptions.module';
 import { RepositoriesModule } from './infrastructure/repositories/repositories.module';
 import { ControllersModule } from './infrastructure/controllers/controllers.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './infrastructure/common/strategies/jwt.strategy';
 
 @Module({
   imports: [
-    EnvironmentConfigModule,
     LoggerModule,
     ExceptionsModule,
     RepositoriesModule,
     ControllersModule,
+    JwtModule.register({}),
   ],
+  providers: [JwtStrategy],
 })
 export class AppModule {}
