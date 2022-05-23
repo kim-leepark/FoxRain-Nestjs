1ac5c9a7842f84318db63ceba166b14101260a0f
Seung jun <poodle2405@gmail.com>
2022-05-23 11:24:31 +0900

    :bookmark_tabs: :: user info usecase 생성

---
+++ src/usecase/user/user-info.ts
@@ -0,0 +1,22 @@
+import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+import { GetUserInfoPresenter } from 'src/presentation/user/user-info.presenter';
+
+export class UserInfoUsecase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly exceptionService: IException,
+  ) {}
+
+  async execute(
+    userId: number,
+    page: number,
+    size: number,
+  ): Promise<GetUserInfoPresenter[]> {
+    const userInfo = await this.userRepository.userInfo(userId, page, size);
+
+    if (userInfo.length === 0) this.exceptionService.notFoundUserException();
+
+    return userInfo;
+  }
+}
