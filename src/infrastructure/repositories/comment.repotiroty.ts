9e37cf7743663f7476eeb763b4ea32dbf7e3f983
Seung jun <poodle2405@gmail.com>
2022-05-23 12:09:28 +0900

    ⚡️ :: createComment 기능 추가

--- src/infrastructure/repositories/comment.repotiroty.ts
+++ src/infrastructure/repositories/comment.repotiroty.ts
@@ -3,11 +3,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CommentRepository } from 'src/domain/repositories/comment.repository';
 import { Repository } from 'typeorm';
 import { CommentTypeOrmEntity } from '../entities/comment.entity';
+import { PostTypeOrmEntity } from '../entities/post.entity';
+import { UserTypeOrmEntity } from '../entities/user.entity';
 
 @Injectable()
 export class DatabaseCommentRepository implements CommentRepository {
   constructor(
     @InjectRepository(CommentTypeOrmEntity)
     private readonly commentEntityRepository: Repository<CommentTypeOrmEntity>,
+    @InjectRepository(UserTypeOrmEntity)
+    private readonly userEntityRepository: Repository<UserTypeOrmEntity>,
+    @InjectRepository(PostTypeOrmEntity)
+    private readonly postEntityRepository: Repository<PostTypeOrmEntity>,
   ) {}
+
+  async createComment(
+    content: string,
+    postId: number,
+    userId: number,
+  ): Promise<void> {
+    const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(
+      userId,
+    );
+    const post: PostTypeOrmEntity = await this.postEntityRepository.findOne(
+      postId,
+    );
+
+    await this.commentEntityRepository.save({
+      content,
+      post,
+      user,
+    });
+  }
 }
