4b02fc099d12e2f72c70c4a82e05479727df8ee0
Seung jun <poodle2405@gmail.com>
2022-05-23 10:04:08 +0900

    :bookmark_tabs: :: user sign up usecase

---
+++ src/usecase/user/sign-up.ts
@@ -0,0 +1,22 @@
+import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+import * as bcrypt from 'bcrypt';
+import { SignUpDto } from '../../presentation/user/user.dto';
+
+export class SignUpUsecase {
+  constructor(
+    private readonly userRepositoy: UserRepository,
+    private readonly exceptionsService: IException,
+  ) {}
+
+  async execute(request: SignUpDto) {
+    const user = await this.userRepositoy.findOne(request.email);
+
+    if (user) this.exceptionsService.userAlreadyExistException();
+
+    const hashPassword = await bcrypt.hash(request.password, 12);
+    request.password = hashPassword;
+
+    this.userRepositoy.singUp(request);
+  }
+}
