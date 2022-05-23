606a4a2473fb767576832cf33b45a6e3cbf53ae2
Seung jun <poodle2405@gmail.com>
2022-05-23 11:22:01 +0900

    :recycle: :: renaming

--- src/domain/model/user.ts
+++ src/domain/model/user.ts
@@ -1,6 +1,10 @@
-export class User {
+export class UserM {
   id: number;
   email: string;
   name: string;
   password: string;
+
+  constructor(obj) {
+    return Object.assign(this, obj);
+  }
 }
