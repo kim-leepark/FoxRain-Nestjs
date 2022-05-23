0abd103bd981b1b254f2e6ef5526f98bd2fad03a
Seung jun <poodle2405@gmail.com>
2022-05-23 19:41:24 +0900

    ⚡️ :: CommentReportUsecase 생성

---
+++ src/usecase/comment/comment-report.ts
@@ -0,0 +1,16 @@
+import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { CommentReportRepository } from 'src/domain/repositories/comment-report.repository';
+
+export class CommentReportUsecase {
+  constructor(private readonly commentReportRepository: CommentReportRepository, private readonly exceptionsService: IException) {}
+
+  async execute(content: string, commentId: number, userId: number) {
+    const reportedComment = await this.commentReportRepository.findOne(commentId, userId);
+
+    console.log(reportedComment);
+
+    if (reportedComment) this.exceptionsService.alreadyReportedCommentException();
+
+    this.commentReportRepository.commentReport(content, commentId, userId);
+  }
+}
