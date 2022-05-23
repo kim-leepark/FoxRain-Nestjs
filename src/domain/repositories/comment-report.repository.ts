2df06d558064ebb61a22aadf4f3e012d50bf3abf
Seung jun <poodle2405@gmail.com>
2022-05-23 20:27:29 +0900

    ⚡️ :: reportedCommentList 기능 추가

--- src/domain/repositories/comment-report.repository.ts
+++ src/domain/repositories/comment-report.repository.ts
@@ -4,4 +4,5 @@ export interface CommentReportRepository {
   findOne(commentId: number, userId: number);
   commentReport(content: string, commentId: number, userId: number): Promise<void>;
   reportedCommentReasonsList(commentId: number): Promise<CommentReportM[]>;
+  reportedCommentList(page: number, size: number): Promise<CommentReportM[]>;
 }
