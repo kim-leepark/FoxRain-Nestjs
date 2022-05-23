12c255c8752dcb13d594a7e83e9ab65c363d96fe
Seung jun <poodle2405@gmail.com>
2022-05-23 10:03:55 +0900

    :bookmark_tabs: :: user login usecase

---
+++ src/usecase/user/login.ts
@@ -0,0 +1,29 @@
+import { IException } from 'src/domain/exceptions/exceptions.interface';
+import { User } from 'src/domain/model/user';
+import { UserRepository } from 'src/domain/repositories/user.repository';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from '../../presentation/user/user.dto';
+
+export class LoginUsecase {
+  constructor(
+    private readonly userRepositoy: UserRepository,
+    private readonly exceptionsService: IException,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async execute(request: LoginDto) {
+    const user: User = await this.userRepositoy.findOne(request.email);
+    const confirmPassword: boolean = await bcrypt.compare(
+      request.password,
+      user.password,
+    );
+
+    if (!user) this.exceptionsService.notFoundUserException();
+    if (!confirmPassword) this.exceptionsService.notConfirmPasswordException();
+
+    const payload = { userId: user.id, type: 'access' };
+
+    return { access_token: this.jwtService.sign(payload) };
+  }
+}
