8375ae9b816aeb4d884a75a4979b8d3d2de51169
Seung jun <poodle2405@gmail.com>
2022-05-23 12:09:53 +0900

    ⚡️ :: CreateCommentUsecase 등록

--- src/infrastructure/usecases-proxy/usecases-proxy.module.ts
+++ src/infrastructure/usecases-proxy/usecases-proxy.module.ts
@@ -1,11 +1,14 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { JwtModule, JwtService } from '@nestjs/jwt';
+import { CreateCommentUsecase } from 'src/usecase/comment/create-comment';
 import { LoginUsecase } from 'src/usecase/user/login';
 import { SignUpUsecase } from 'src/usecase/user/sign-up';
 import { UserInfoUsecase } from 'src/usecase/user/user-info';
 import { ExceptionsModule } from '../exceptions/exceptions.module';
 import { ExceptionsService } from '../exceptions/exceptions.service';
 import { LoggerModule } from '../logger/logger.module';
+import { DatabaseCommentRepository } from '../repositories/comment.repotiroty';
+import { DatabasePostRepository } from '../repositories/post.repotiroty';
 import { RepositoriesModule } from '../repositories/repositories.module';
 import { DatabaseUserRepository } from '../repositories/user.repotiroty';
 
@@ -52,8 +55,31 @@ export class UsecasesProxyDynamicModule {
             exceptionsService: ExceptionsService,
           ) => new UserInfoUsecase(databaseUserRepository, exceptionsService),
         },
+        {
+          inject: [
+            DatabasePostRepository,
+            DatabaseCommentRepository,
+            ExceptionsService,
+          ],
+          provide: CreateCommentUsecase,
+          useFactory: (
+            databasePostRepository: DatabasePostRepository,
+            databaseCommentRepository: DatabaseCommentRepository,
+            exceptionsService: ExceptionsService,
+          ) =>
+            new CreateCommentUsecase(
+              databasePostRepository,
+              databaseCommentRepository,
+              exceptionsService,
+            ),
+        },
       ],
-      exports: [SignUpUsecase, LoginUsecase, UserInfoUsecase],
+      exports: [
+        SignUpUsecase,
+        LoginUsecase,
+        UserInfoUsecase,
+        CreateCommentUsecase,
+      ],
     };
   }
 }
