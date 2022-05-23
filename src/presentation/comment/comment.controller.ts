410e1ac9c04b38ffcf7d84995420729d4fe7e609
Seung jun <poodle2405@gmail.com>
2022-05-23 19:18:35 +0900

    ⚡️ :: Get /comment/:postId?page=&size= 추가

--- src/presentation/comment/comment.controller.ts
+++ src/presentation/comment/comment.controller.ts
@@ -1,25 +1,31 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Inject,
   Param,
   ParseIntPipe,
   Post,
+  Query,
   Scope,
   UseGuards,
 } from '@nestjs/common';
 import { REQUEST } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IUserReqeust } from 'src/domain/interfaces/request.interface';
+import { CommentM } from 'src/domain/model/comment';
 import { CreateCommentUsecase } from 'src/usecase/comment/create-comment';
+import { GetCommentListUsecase } from 'src/usecase/comment/get-comment-list';
 
 @Controller({ path: '/comment', scope: Scope.REQUEST })
 export class CommnetController {
   constructor(
     @Inject(CreateCommentUsecase)
     private readonly createCommentUsecase: CreateCommentUsecase,
+    @Inject(GetCommentListUsecase)
+    private readonly getCommentListUsecase: GetCommentListUsecase,
     @Inject(REQUEST)
     private readonly request: IUserReqeust,
   ) {}
@@ -36,5 +42,14 @@ export class CommnetController {
       postId,
       this.request.user.sub,
     );
+  }
+
+  @Get('/:postId')
+  commentList(
+    @Param('postId', ParseIntPipe) postId: number,
+    @Query('page', ParseIntPipe) page: number,
+    @Query('size', ParseIntPipe) size: number,
+  ): Promise<CommentM[]> {
+    return this.getCommentListUsecase.execute(postId, page, size);
   }
 }
