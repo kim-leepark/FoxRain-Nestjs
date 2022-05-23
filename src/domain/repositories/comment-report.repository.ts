e3d7ec8ec1a8d1c73eb9c22141a41f863949ed3f
Seung jun <poodle2405@gmail.com>
2022-05-23 20:06:07 +0900

    ⚡️ :: reportedCommentReasonsList 기능 추가

--- src/domain/repositories/comment-report.repository.ts
+++ src/domain/repositories/comment-report.repository.ts
@@ -1,4 +1,7 @@
+import { CommentReportM } from '../model/comment-report';
+
 export interface CommentReportRepository {
   findOne(commentId: number, userId: number);
   commentReport(content: string, commentId: number, userId: number): Promise<void>;
+  reportedCommentReasonsList(commentId: number): Promise<CommentReportM[]>;
 }
