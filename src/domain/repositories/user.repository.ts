d7274797c115edd58298fa12452cb0570f5bfa46
Seung jun <poodle2405@gmail.com>
2022-05-23 10:06:27 +0900

    ⚡️ :: user sign-up, findOne 기능 추가

--- src/domain/repositories/user.repository.ts
+++ src/domain/repositories/user.repository.ts
@@ -1,1 +1,6 @@
-export interface UserRepository {}
+import { SignUpDto } from 'src/presentation/user/user.dto';
+
+export interface UserRepository {
+  findOne(email: string);
+  singUp(request: SignUpDto): Promise<void>;
+}
