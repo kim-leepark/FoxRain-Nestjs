56223cc206b44b7745f67174acff9419601129cf
Seung jun <poodle2405@gmail.com>
2022-05-23 20:28:12 +0900

    ⚡️ :: CommentReportController 추가

--- src/infrastructure/controllers/controllers.module.ts
+++ src/infrastructure/controllers/controllers.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommnetController } from 'src/presentation/comment/comment.controller';
+import { CommnetReportController } from 'src/presentation/comment_report/comment-report.controller';
 import { UserController } from 'src/presentation/user/user.controller';
 import { UsecasesProxyDynamicModule } from '../usecases-proxy/usecases-proxy.module';
 
 @Module({
   imports: [ConfigModule, UsecasesProxyDynamicModule.register()],
-  controllers: [UserController, CommnetController],
+  controllers: [UserController, CommnetController, CommnetReportController],
 })
 export class ControllersModule {}
