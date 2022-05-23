ad8ecc836a238eec85dbaa8ac60a30a6a937f809
Seung jun <poodle2405@gmail.com>
2022-05-23 19:18:54 +0900

    ⚡️ :: findCommentList 기능 추가

--- src/infrastructure/repositories/comment.repotiroty.ts
+++ src/infrastructure/repositories/comment.repotiroty.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CommentM } from 'src/domain/model/comment';
 import { CommentRepository } from 'src/domain/repositories/comment.repository';
 import { Repository } from 'typeorm';
+import { CommentReportTypeOrmEntity } from '../entities/comment-report.entity';
 import { CommentTypeOrmEntity } from '../entities/comment.entity';
 import { PostTypeOrmEntity } from '../entities/post.entity';
 import { UserTypeOrmEntity } from '../entities/user.entity';
@@ -11,28 +13,45 @@ export class DatabaseCommentRepository implements CommentRepository {
   constructor(
     @InjectRepository(CommentTypeOrmEntity)
     private readonly commentEntityRepository: Repository<CommentTypeOrmEntity>,
+    @InjectRepository(CommentReportTypeOrmEntity)
+    private readonly commentReportEntityRepository: Repository<CommentReportTypeOrmEntity>,
     @InjectRepository(UserTypeOrmEntity)
     private readonly userEntityRepository: Repository<UserTypeOrmEntity>,
     @InjectRepository(PostTypeOrmEntity)
     private readonly postEntityRepository: Repository<PostTypeOrmEntity>,
   ) {}
 
-  async createComment(
-    content: string,
-    postId: number,
-    userId: number,
-  ): Promise<void> {
-    const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(
-      userId,
-    );
-    const post: PostTypeOrmEntity = await this.postEntityRepository.findOne(
-      postId,
-    );
+  async createComment(content: string, postId: number, userId: number): Promise<void> {
+    const user: UserTypeOrmEntity = await this.userEntityRepository.findOne(userId);
+    const post: PostTypeOrmEntity = await this.postEntityRepository.findOne(postId);
 
     await this.commentEntityRepository.save({
       content,
       post,
       user,
     });
+  }
+
+  async findCommentList(postId: number, page: number, size: number): Promise<CommentM[]> {
+    const comments: any[] = await this.commentEntityRepository
+      .createQueryBuilder('comment')
+      .select('comment.content', 'content')
+      .addSelect('SUBSTR(comment.created_at, 1, 10)', 'createdAt')
+      .addSelect('commentReport.id', 'reportedComment')
+      .offset((page - 1) * size)
+      .limit(size)
+      .where('post.id = :id', { id: postId })
+      .orderBy('comment.created_at', 'DESC')
+      .leftJoin('comment.post', 'post')
+      .leftJoin('comment.comment_report', 'commentReport')
+      .groupBy('comment.id')
+      .getRawMany();
+
+    return Promise.all(
+      comments.map(async (comment) => {
+        comment.reportedComment = comment.reportedComment != null ? true : false;
+        return new CommentM(comment);
+      }),
+    );
   }
 }
