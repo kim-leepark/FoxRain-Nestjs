bcdee6feb8d9d7cc221fd94cbb783eec84ff5779
Seung jun <poodle2405@gmail.com>
2022-05-23 12:09:06 +0900

    ⚡️ :: CommentController 추가

--- src/infrastructure/controllers/controllers.module.ts
+++ src/infrastructure/controllers/controllers.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CommnetController } from 'src/presentation/comment/comment.controller';
 import { UserController } from 'src/presentation/user/user.controller';
 import { UsecasesProxyDynamicModule } from '../usecases-proxy/usecases-proxy.module';
 
 @Module({
   imports: [ConfigModule, UsecasesProxyDynamicModule.register()],
-  controllers: [UserController],
+  controllers: [UserController, CommnetController],
 })
 export class ControllersModule {}
