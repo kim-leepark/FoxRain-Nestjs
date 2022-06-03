5b187e00536274ccf1d3cbc8e2e7cdf5fb335e82
Seung jun <poodle2405@gmail.com>
2022-06-03 10:46:10 +0900

    ⚡️ :: cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionFilter } from './infrastructure/common/filters/exception.filter';
@@ -8,9 +9,24 @@ import { LoggerService } from './infrastructure/logger/logger.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+
   app.useGlobalFilters(new AllExceptionFilter(new LoggerService()));
+
   app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
   app.useGlobalInterceptors(new ResponseInterceptor());
+
   await app.listen(3002);
 }
 bootstrap();
