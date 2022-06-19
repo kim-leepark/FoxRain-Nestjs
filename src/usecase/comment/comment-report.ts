67a1b41fa4691ebe8fd05f2021d029a49923f1c0
Seung jun <poodle2405@gmail.com>
2022-06-20 08:49:28 +0900

    ⚡️ :: exception handling 추가

--- src/usecase/comment/comment-report.ts
+++ src/usecase/comment/comment-report.ts
@@ -5,9 +5,11 @@ export class CommentReportUsecase {
   constructor(private readonly commentReportRepository: CommentReportRepository, private readonly exceptionsService: IException) {}
 
   async execute(content: string, commentId: number, userId: number) {
-    const reportedComment = await this.commentReportRepository.findOne(commentId, userId);
+    const reportedComment = await this.commentReportRepository.findComment(commentId, userId);
+    const comment = await this.commentReportRepository.findOne(commentId);
 
     if (reportedComment) this.exceptionsService.alreadyReportedCommentException();
+    if (!comment) this.exceptionsService.commentNotFoundException();
 
     this.commentReportRepository.commentReport(content, commentId, userId);
   }
