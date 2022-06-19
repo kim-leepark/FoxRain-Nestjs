e19c26cc6bc3984edf8baa81026820e76a526989
Seung jun <poodle2405@gmail.com>
2022-06-20 08:47:54 +0900

    ⚡️ :: add findComment

--- src/domain/repositories/comment-report.repository.ts
+++ src/domain/repositories/comment-report.repository.ts
@@ -1,7 +1,8 @@
 import { CommentReportM } from '../model/comment-report';
 
 export interface CommentReportRepository {
-  findOne(commentId: number, userId: number);
+  findOne(commentId: number);
+  findComment(commentId: number, userId: number);
   commentReport(content: string, commentId: number, userId: number): Promise<void>;
   reportedCommentReasonsList(commentId: number): Promise<CommentReportM[]>;
   reportedCommentList(page: number, size: number): Promise<CommentReportM[]>;
